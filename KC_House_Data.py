6e4711b326ae1d7332c0c2448d351970744701a0
mayurgpt07 <mayurgpt07@gmail.com>
2019-10-17 20:25:18 -0700

    Colinearity Problems

--- KC_House_Data.py
+++ KC_House_Data.py
@@ -6,6 +6,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 import statsmodels.formula.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.tools.tools import add_constant
 
 
 train_data = pd.read_csv("./data/kc_house_data.csv", sep = ",", header = 0)
@@ -16,9 +18,10 @@ train_data['dateInFormat'] = train_data['dateInFormat'].apply(lambda x: datetime
 train_data['YearSold'] = train_data['dateInFormat'].apply(lambda x: int(x.year))
 train_data['HomeAgeinYear'] = train_data['YearSold'] - train_data['yr_built']
 train_data['RenovatedafterYears'] = train_data['yr_built'] - train_data['yr_renovated']
+train_data['WaterFrontPresent'] = train_data['waterfront'].apply(lambda x: 1 if x == 1 else -1)
 #Check the correlation matrix with price
 
-
+#print(train_data['WaterFrontPresent'])
 train_data.corr().loc[:,'id':'price'].style.background_gradient(cmap='coolwarm', axis=None)
 
 #pd.plotting.scatter_matrix(train_data.loc[:,'price':'floors'], alpha = 0.2, figsize = (20, 20), diagonal = 'kde')
@@ -30,18 +33,21 @@ train_data['Log_sqftlot15'] = train_data['sqft_lot15'].apply(lambda x: np.log(x)
 
 #plt.hist(train_data['sqft_lot'], color = "red")
 #plt.hist(train_data['sqft_lot'], color = "skyblue")
-#plt.hist(train_data['price'], color = 'red')
-#plt.hist(train_data['sqft_living'], color = 'brown')
-plt.hist(train_data['RenovatedafterYears'], color = 'green')
-plt.show()
+#plt.hist(train_data['RenovatedafterYears'], color = 'green')
+#plt.show()
 
 columnsToTrain = ['bedrooms', 'bathrooms', 'sqft_living', 'floors', 'waterfront', 'view', 'condition', 'grade',
        'sqft_above', 'HomeAgeinYear', 'RenovatedafterYears',
-       'lat', 'long', 'sqft_living15','Log_sqftlot15',
-       'Log_sqftlot']
+       'lat', 'long','Log_sqftlot']
 
 
 X, y = train_data.loc[:, columnsToTrain], train_data.loc[:, 'Log_price']
+
+vif = pd.DataFrame()
+New_X = add_constant(X)
+vif['VIF Factors']  = [variance_inflation_factor(New_X.values, i) for i in range(New_X.shape[1])]
+vif['Columns'] = New_X.columns
+print(vif)
 
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, y, test_size = 0.30)
 
@@ -54,7 +60,7 @@ print(fittedModel.score(X_train, Y_train))
 print(fittedModel.coef_)
 
 
-formuala = 'Log_price ~ bedrooms+bathrooms+sqft_living+Log_sqftlot+floors+waterfront+view+condition+grade+sqft_above+HomeAgeinYear+RenovatedafterYears+lat+long+sqft_living15+Log_sqftlot15'
+formuala = 'Log_price ~ bedrooms+bathrooms+sqft_living+Log_sqftlot+floors+waterfront+view+condition+grade+sqft_above+HomeAgeinYear+RenovatedafterYears+lat+long'
 statisticalModel = sm.ols(formuala, data = train_data)
 statsfitted = statisticalModel.fit()
 print(statsfitted.summary())

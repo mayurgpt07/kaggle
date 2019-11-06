6a840a8c8e8c6273fc4120c96777c050cc827c77
mayurgpt07 <mayurgpt07@gmail.com>
2019-11-06 00:27:59 -0800

    Test Data replicated

--- NaturalToxicLanguage.py
+++ NaturalToxicLanguage.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer as tfidf
 import re
 
 train_data = pd.read_csv('./jigsaw-toxic-comment-classification-challenge/train.csv', sep = ',', header = 0)
@@ -13,7 +14,7 @@ train_data['new_Comment_Text'] = train_data['comment_text'].apply(lambda x: re.s
 test_data['new_Comment_Text'] = test_data['comment_text'].apply(lambda x:re.sub('\s+',' ',x.strip()))
 
 train_data['new_Comment_Text'] = train_data['new_Comment_Text'].apply(lambda x: re.sub('\ |\?|\.|\!|\/|\;|\:|\=|\"|\:',' ', x))
-
+test_data['new_Comment_Text'] = train_data['new_Comment_Text'].apply(lambda x: re.sub('\ |\?|\.|\!|\/|\;|\:|\=|\"|\:',' ', x))
  
 train_data['CommentTokenize'] = train_data['new_Comment_Text'].apply(lambda x: word_tokenize(x))
 test_data['CommentTokenize'] = test_data['new_Comment_Text'].apply(lambda x:word_tokenize(x))
@@ -26,8 +27,16 @@ for i in train_data['CommentTokenize']:
 	for k in i:
 		if k not in stopwordsList:
 			wordList = wordList + ' '+k
-	sentenceList.append(wordList)
+	sentenceList.append(wordList.strip())
 #wordList.clear()
 
+for i in test_data['CommentTokenize']:
+	wordListTest = ''
+	for k in i:
+		if k not in stopwordsList:
+			wordListTest = wordListTest + ' ' + k
+	sentenceListTest.append(wordListTest.strip())
+
 train_data['RemovedStopWords'] = pd.Series(sentenceList)
+test_data['RemovedStopWords'] = pd.Series(sentenceListTest)
 print(train_data.head())

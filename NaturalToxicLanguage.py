fc2c7eefc8f7d7bab9ef1a845b504521393f93db
mayurgpt07 <mayurgpt07@gmail.com>
2019-11-08 18:16:13 -0800

    Features Created

--- NaturalToxicLanguage.py
+++ NaturalToxicLanguage.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from pattern.en import suggest
 import re
 
 def reduce_lengthening(text):
@@ -13,6 +14,7 @@ def reduce_lengthening(text):
     return pattern.sub(r"\1\1", text)
 
 train_data = pd.read_csv('./jigsaw-toxic-comment-classification-challenge/train.csv', sep = ',', header = 0)
+train_data['comment_text'].fillna('unknown', inplace = True)
 #test_data = pd.read_csv('./jigsaw-toxic-comment-classification-challenge/test.csv', sep = ',', header = 0)
 
 stopwordsList = set(stopwords.words('english'))
@@ -46,42 +48,80 @@ for i in train_data['CommentTokenize']:
 #	sentenceListTest.append(wordListTest.lstrip())
 
 train_data['RemovedStopWords'] = pd.Series(sentenceList)
+train_data['RemovedStopWords'] = train_data['RemovedStopWords'].apply(lambda x: re.sub('\s+', ' ', x.strip()))
 
-toxic_data = train_data[train_data['toxic'] == 1]['RemovedStopWords']
+toxic_data = pd.DataFrame()
+Severetoxic_data = pd.DataFrame()
+Obscenetoxic_data = pd.DataFrame()
+Threattoxic_data = pd.DataFrame()
+Insulttoxic_data = pd.DataFrame()
+Identitytoxic_data = pd.DataFrame()
 
-Severetoxic_data = train_data[train_data['severe_toxic'] == 1]['RemovedStopWords']
+toxic_data['Comments'] = train_data[train_data['toxic'] == 1]['RemovedStopWords']
+toxic_data['lengthOfLength'] = toxic_data['Comments'].apply(lambda x: len(x.strip()))
+toxic_data['numberOfWords'] = toxic_data['Comments'].apply(lambda x: len(x.split()))
+plt.hist(toxic_data['lengthOfLength'], color = "red")
+plt.show()
 
-Obscenetoxic_data = train_data[train_data['obscene'] == 1]['RemovedStopWords']
+print(toxic_data.head())
 
-Threattoxic_data = train_data[train_data['threat'] == 1]['RemovedStopWords']
+Severetoxic_data['Comments'] = train_data[train_data['severe_toxic'] == 1]['RemovedStopWords']
+Severetoxic_data['lengthOfLength'] = Severetoxic_data['Comments'].apply(lambda x: len(x.strip()))
+Severetoxic_data['numberOfWords'] = Severetoxic_data['Comments'].apply(lambda x: len(x.split()))
+plt.hist(Severetoxic_data['lengthOfLength'], color = "red")
+plt.show()
 
-Insulttoxic_data = train_data[train_data['insult'] == 1]['RemovedStopWords']
+Obscenetoxic_data['Comments'] = train_data[train_data['obscene'] == 1]['RemovedStopWords']
+Obscenetoxic_data['lengthOfLength'] = Obscenetoxic_data['Comments'].apply(lambda x: len(x.strip()))
+Obscenetoxic_data['numberOfWords'] = Obscenetoxic_data['Comments'].apply(lambda x: len(x.split()))
+plt.hist(Obscenetoxic_data['lengthOfLength'], color = "red")
+plt.show()
 
-Identitytoxic_data = train_data[train_data['identity_hate'] == 1]['RemovedStopWords']
+
+Threattoxic_data['Comments'] = train_data[train_data['threat'] == 1]['RemovedStopWords']
+Threattoxic_data['lengthOfLength'] = Threattoxic_data['Comments'].apply(lambda x: len(x.strip()))
+Threattoxic_data['numberOfWords'] = Threattoxic_data['Comments'].apply(lambda x: len(x.split()))
+plt.hist(Threattoxic_data['lengthOfLength'], color = "red")
+plt.show()
+
+
+Insulttoxic_data['Comments'] = train_data[train_data['insult'] == 1]['RemovedStopWords']
+Insulttoxic_data['lengthOfLength'] = Insulttoxic_data['Comments'].apply(lambda x: len(x.strip()))
+Insulttoxic_data['numberOfWords'] = Insulttoxic_data['Comments'].apply(lambda x: len(x.split()))
+plt.hist(Insulttoxic_data['lengthOfLength'], color = "red")
+plt.show()
+
+
+Identitytoxic_data['Comments'] = train_data[train_data['identity_hate'] == 1]['RemovedStopWords']
+Identitytoxic_data['lengthOfLength'] = Identitytoxic_data['Comments'].apply(lambda x: len(x.strip()))
+Identitytoxic_data['numberOfWords'] = Identitytoxic_data['Comments'].apply(lambda x: len(x.split()))
+plt.hist(Identitytoxic_data['lengthOfLength'], color = "red")
+plt.show()
+
 
 empty_string = ''
-for i in toxic_data:
-	empty_string = empty_string + ' ' + i
+for i in toxic_data['Comments']:
+	empty_string = empty_string.strip() + ' ' + i.strip()
 
 empty_string1 = ''
-for i in Severetoxic_data:
-	empty_string1 = empty_string1 + ' ' + i
+for i in Severetoxic_data['Comments']:
+	empty_string1 = empty_string1.strip() + ' ' + i.strip()
 
 empty_string2 = ''
-for i in Obscenetoxic_data:
-	empty_string2 = empty_string2 + ' ' + i
+for i in Obscenetoxic_data['Comments']:
+	empty_string2 = empty_string2.strip() + ' ' + i.strip()
 
 empty_string3 = ''
-for i in Threattoxic_data:
-	empty_string3 = empty_string3 + ' ' + i
+for i in Threattoxic_data['Comments']:
+	empty_string3 = empty_string3.strip() + ' ' + i.strip()
 
 empty_string4 = ''
-for i in Insulttoxic_data:
-	empty_string4 = empty_string4 + ' ' + i
+for i in Insulttoxic_data['Comments']:
+	empty_string4 = empty_string4.strip() + ' ' + i.strip()
 
 empty_string5 = ''
-for i in Identitytoxic_data:
-	empty_string5 = empty_string5 + ' ' + i
+for i in Identitytoxic_data['Comments']:
+	empty_string5 = empty_string5.strip() + ' ' + i.strip()
 
 wordcloud = WordCloud(width = 900, height = 900, 
                 background_color ='white',
